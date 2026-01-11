1c9d0a83d0ae79ca6116f0e7dd7dd661586bdb4d
snehameganathan <snehameganath@gmail.com>
2026-01-11 05:46:41 +0530

    Initial commit - Bulk Mail App

---
+++ backend/index.js
@@ -0,0 +1,87 @@
+require("dotenv").config();
+
+const express = require("express")
+const cors = require("cors")
+const nodemailer = require("nodemailer");
+const mongoose = require("mongoose")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI).then(function(){
+    console.log("Connected to DB")
+}).catch(function(){
+    console.log("Failed to connect DB")
+})
+
+const credentialSchema = new mongoose.Schema({
+    user: String,
+    pass: String
+  });
+
+const credential = mongoose.model("credential",credentialSchema,"bulkmail")
+
+// Create a transporter using Ethereal test credentials.
+// For production, replace with your actual SMTP server details.
+
+
+
+
+app.post("/sendmail",function(req,res){
+
+    var msg = req.body.msg
+    var emailList = req.body.emailList
+
+    credential.find().then(function(data){
+        
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+            user: data[0].user,
+            pass: data[0].pass,
+          },
+        });
+    
+        new Promise(async function(resolve,reject){
+            try
+            {
+                for(var i=0; i<emailList.length;i++)
+                {
+                    await transporter.sendMail(
+                        {
+                            from: data[0].user,
+                            to:emailList[i],
+                            subject: " Message from Bulk Mail App",
+                            text:msg
+                        }            
+                    )  
+                
+                }
+                resolve("Success")
+            }
+            catch(error){
+                reject("Failed")
+            }
+        }).then(function(){
+            res.send(true)
+        }).catch(function(){
+            res.send(false)
+        })
+        
+    }).catch(function(error){
+        console.log(error)
+    })
+    
+
+    
+
+    
+})
+    
+
+app.listen(5000, function(){
+    console.log("Server Started......")
+})
+
