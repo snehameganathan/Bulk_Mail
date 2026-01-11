f29dea092e61c77133a04a1a016b01479bf1619a
snehameganathan <snehameganath@gmail.com>
2026-01-11 05:51:25 +0530

    Initial commit - Bulk Mail App

--- backend/index.js
+++ backend/index.js
@@ -81,7 +81,9 @@ app.post("/sendmail",function(req,res){
 })
     
 
-app.listen(5000, function(){
-    console.log("Server Started......")
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on", PORT);
+});
+
 
